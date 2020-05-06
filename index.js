d811646103ae64434316231ec890fcc3f08623e7
i-shulenin <ilya@shulenin.com>
2020-05-06 09:38:07 +0300

    Линейная диаграмма

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-import './style.css';
+import "./style.css";
 
-import picasso from 'picasso.js';
-import picassoQ from 'picasso-plugin-q';
+import picasso from "picasso.js";
+import picassoQ from "picasso-plugin-q";
 
 picasso.use(picassoQ);
 
 const layout = {
   qHyperCube: {
     qSize: {
-      qcx: 3,
-      qcy: 12
+      qcx: 2,
+      qcy: 10
     },
     qDimensionInfo: [
       {
-        qFallbackTitle: "Месяц",
-        qApprMaxGlyphCount: 8,
-        qCardinal: 12,
+        qFallbackTitle: "Федеральный округ",
+        qApprMaxGlyphCount: 35,
+        qCardinal: 9,
         qSortIndicator: "A",
-        qGroupFallbackTitles: ["Месяц"],
+        qGroupFallbackTitles: ["Федеральный округ", "Наименование региона"],
         qGroupPos: 0,
         qStateCounts: {
           qLocked: 0,
           qSelected: 0,
-          qOption: 12,
+          qOption: 9,
           qDeselected: 0,
           qAlternative: 0,
           qExcluded: 0,
           qSelectedExcluded: 0,
           qLockedExcluded: 0
         },
-        qTags: ["$text", "$keypart"],
+        qTags: ["$text"],
         qDimensionType: "D",
-        qGrouping: "N",
+        qGrouping: "H",
         qNumFormat: {
           qType: "R",
           qnDec: 14,
@@ -40,88 +40,64 @@ const layout = {
           qDec: ","
         },
         qIsAutoFormat: true,
-        qGroupFieldDefs: ["Месяц"],
+        qGroupFieldDefs: ["Федеральный округ", "Наименование региона"],
         qMin: "NaN",
         qMax: "NaN",
         qAttrExprInfo: [],
         qAttrDimInfo: [],
         qCardinalities: {
-          qCardinal: 12,
-          qHypercubeCardinal: 12
+          qCardinal: 9,
+          qHypercubeCardinal: 10,
+          qAllValuesCardinal: -1
         },
-        autoSort: false,
-        cId: "QTSeaT",
-        othersLabel: "Others"
+        qLibraryId: "ennD",
+        title: "Округ\\Регион",
+        coloring: {
+          changeHash: "0.1639923224171136"
+        },
+        autoSort: true,
+        cId: "PLPPusy",
+        othersLabel: "Другие"
       }
     ],
     qMeasureInfo: [
       {
-        qFallbackTitle: "Количество работников",
+        qFallbackTitle: "Дефицит (физлиц)  на 19.04.2020 21:00:00",
         qApprMaxGlyphCount: 7,
         qCardinal: 0,
         qSortIndicator: "D",
         qNumFormat: {
-          qType: "F",
-          qnDec: 2,
+          qType: "R",
+          qnDec: 0,
           qUseThou: 0,
-          qFmt: "# ##0",
+          qFmt: "##############",
           qDec: ",",
           qThou: " "
         },
-        qMin: 49370,
-        qMax: 123354,
+        qMin: -200673,
+        qMax: 0,
+        qIsAutoFormat: true,
         qAttrExprInfo: [],
         qAttrDimInfo: [],
+        qLibraryId: "Krs",
+        qTrendLines: [],
+        coloring: {},
         autoSort: true,
-        cId: "GJpES",
+        cId: "eGrpnm",
         numFormatFromTemplate: true,
         series: {
           type: "bar",
           axis: 0,
           marker: "circle",
           markerFill: true
-        },
-        isCustomFormatted: false
-      },
-      {
-        qFallbackTitle: "Количество МО",
-        qApprMaxGlyphCount: 5,
-        qCardinal: 0,
-        qSortIndicator: "D",
-        qNumFormat: {
-          qType: "F",
-          qnDec: 2,
-          qUseThou: 0,
-          qFmt: "# ##0",
-          qDec: ",",
-          qThou: " "
-        },
-        qMin: 296,
-        qMax: 522,
-        qAttrExprInfo: [],
-        qAttrDimInfo: [],
-        autoSort: true,
-        cId: "dCqwpu",
-        numFormatFromTemplate: true,
-        series: {
-          axis: 1,
-          type: "line"
-        },
-        isCustomFormatted: false
+        }
       }
     ],
-    qEffectiveInterColumnSortOrder: [0, 1, 2],
+    qEffectiveInterColumnSortOrder: [0, 1],
     qGrandTotalRow: [
       {
-        qText: "992 013",
-        qNum: 992013,
-        qElemNumber: -1,
-        qState: "X",
-        qIsTotalCell: true
-      },
-      {
-        qText: "5 898",
-        qNum: 5898,
+        qText: "-599662",
+        qNum: -599662,
         qElemNumber: -1,
         qState: "X",
         qIsTotalCell: true
@@ -132,244 +108,130 @@ const layout = {
         qMatrix: [
           [
             {
-              qText: "январь",
-              qNum: "NaN",
-              qElemNumber: 10,
-              qState: "O"
-            },
-            {
-              qText: "80 201",
-              qNum: 80201,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "519",
-              qNum: 519,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "февраль",
-              qNum: "NaN",
-              qElemNumber: 9,
-              qState: "O"
-            },
-            {
-              qText: "67 148",
-              qNum: 67148,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "514",
-              qNum: 514,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "март",
-              qNum: "NaN",
-              qElemNumber: 5,
-              qState: "O"
-            },
-            {
-              qText: "106 192",
-              qNum: 106192,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "512",
-              qNum: 512,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "апрель",
+              qText: "Дальневосточный федеральный округ",
               qNum: "NaN",
               qElemNumber: 1,
               qState: "O"
             },
             {
-              qText: "80 236",
-              qNum: 80236,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "520",
-              qNum: 520,
+              qText: "-22759",
+              qNum: -22759,
               qElemNumber: 0,
               qState: "L"
             }
           ],
           [
             {
-              qText: "май",
-              qNum: "NaN",
-              qElemNumber: 4,
-              qState: "O"
-            },
-            {
-              qText: "123 354",
-              qNum: 123354,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "522",
-              qNum: 522,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "июнь",
-              qNum: "NaN",
-              qElemNumber: 3,
-              qState: "O"
-            },
-            {
-              qText: "106 836",
-              qNum: 106836,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "520",
-              qNum: 520,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "июль",
-              qNum: "NaN",
-              qElemNumber: 2,
-              qState: "O"
-            },
-            {
-              qText: "106 548",
-              qNum: 106548,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "514",
-              qNum: 514,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "август",
-              qNum: "NaN",
-              qElemNumber: 0,
-              qState: "O"
-            },
-            {
-              qText: "75 688",
-              qNum: 75688,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "499",
-              qNum: 499,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "сентябрь",
+              qText: "МО Федерального подчинения",
               qNum: "NaN",
               qElemNumber: 8,
               qState: "O"
             },
             {
-              qText: "55 910",
-              qNum: 55910,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "504",
-              qNum: 504,
+              qText: "-72577",
+              qNum: -72577,
               qElemNumber: 0,
               qState: "L"
             }
           ],
           [
             {
-              qText: "октябрь",
-              qNum: "NaN",
-              qElemNumber: 7,
-              qState: "O"
-            },
-            {
-              qText: "64 784",
-              qNum: 64784,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "492",
-              qNum: 492,
-              qElemNumber: 0,
-              qState: "L"
-            }
-          ],
-          [
-            {
-              qText: "ноябрь",
+              qText: "Приволжский федеральный округ",
               qNum: "NaN",
               qElemNumber: 6,
               qState: "O"
             },
             {
-              qText: "75 746",
-              qNum: 75746,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "486",
-              qNum: 486,
+              qText: "-82268",
+              qNum: -82268,
               qElemNumber: 0,
               qState: "L"
             }
           ],
           [
             {
-              qText: "декабрь",
+              qText: "Северо-Западный федеральный округ",
               qNum: "NaN",
-              qElemNumber: 11,
+              qElemNumber: 2,
               qState: "O"
             },
             {
-              qText: "49 370",
-              qNum: 49370,
-              qElemNumber: 0,
-              qState: "L"
-            },
-            {
-              qText: "296",
-              qNum: 296,
+              qText: "-38269",
+              qNum: -38269,
               qElemNumber: 0,
               qState: "L"
             }
-          ]
+          ],
+          [
+            {
+              qText: "Северо-Кавказский федеральный округ",
+              qNum: "NaN",
+              qElemNumber: 5,
+              qState: "O"
+            },
+            {
+              qText: "-39311",
+              qNum: -39311,
+              qElemNumber: 0,
+              qState: "L"
+            }
+          ],
+          [
+            {
+              qText: "Сибирский федеральный округ",
+              qNum: "NaN",
+              qElemNumber: 0,
+              qState: "O"
+            },
+            {
+              qText: "-57501",
+              qNum: -57501,
+              qElemNumber: 0,
+              qState: "L"
+            }
+          ],
+          [
+            {
+              qText: "Уральский федеральный округ",
+              qNum: "NaN",
+              qElemNumber: 7,
+              qState: "O"
+            },
+            {
+              qText: "-38952",
+              qNum: -38952,
+              qElemNumber: 0,
+              qState: "L"
+            }
+          ],
+          [
+            {
+              qText: "Центральный федеральный округ",
+              qNum: "NaN",
+              qElemNumber: 4,
+              qState: "O"
+            },
+            {
+              qText: "-200673",
+              qNum: -200673,
+              qElemNumber: 0,
+              qState: "L"
+            }
+          ],
+          [
+            {
+              qText: "Южный федеральный округ",
+              qNum: "NaN",
+              qElemNumber: 3,
+              qState: "O"
+            },
+            {
+              qText: "-47352",
+              qNum: -47352,
+              qElemNumber: 0,
+              qState: "L"
+            }
+          ],
         ],
         qTails: [
           {
@@ -380,8 +242,8 @@ const layout = {
         qArea: {
           qLeft: 0,
           qTop: 0,
-          qWidth: 3,
-          qHeight: 12
+          qWidth: 2,
+          qHeight: 10
         }
       }
     ],
@@ -394,10 +256,20 @@ const layout = {
   }
 };
 
+const dataset = picasso.data('q')({
+  key: 'qHyperCube',
+  data: layout.qHyperCube,
+});
+
+console.log('%c*** qDimensionInfo/0 ***', 'background: #0000FF; color: #FFFFFF; font-weight: bold;');
+console.log(dataset.extract({ field: 'qDimensionInfo/0' }));
+console.log('%c*** qMeasureInfo/0 ***', 'background: #0000EE; color: #FFFFFF; font-weight: bold;');
+console.log(dataset.extract({ field: 'qMeasureInfo/0' }));
+
 const data = [
   {
-    type: 'q',
-    key: 'qHyperCube',
+    type: "q",
+    key: "qHyperCube",
     data: layout.qHyperCube
   }
 ];
@@ -405,41 +277,64 @@ const data = [
 const settings = {
   scales: {
     x: {
-      data: { field: 'qMeasureInfo/0' },
-      expand: 0.1
+      data: {
+        extract: {
+          field: 'qDimensionInfo/0'
+        } 
+      }
+    },
+    y: {
+      data: {
+        extract: {
+          field: 'qMeasureInfo/0'
+        } 
+      }
     }
   },
   components: [
     {
       type: 'axis',
       dock: 'bottom',
-      scale: 'x'
+      scale: 'x',
+    }, {
+      type: 'axis',
+      dock: 'left',
+      scale: 'y',
     },
     {
-      key: 'points',
-      type: 'point',
+      key: 'lines',
+      type: 'line',
       data: {
         extract: {
           field: 'qDimensionInfo/0',
           props: {
-            fill: {
-              field: 'qDimensionInfo/1/qAttrExprInfo/0',
-              value: v => v.qText
-            },
-            end: { field: 'qMeasureInfo/0' }
+            v: { field: 'qMeasureInfo/0' }
           }
         }
       },
       settings: {
-        x: { scale: 'x', ref: 'end' },
-        fill: { ref: 'fill' }
+        coordinates: {
+          major: { scale: 'x' },
+          minor: { scale: 'y', ref: 'v' }
+        },
+        layers: {
+          line: {},
+          curve: 'monotone',
+          line: {
+            show: false
+          },
+          area: {}
+        }
       }
     }
   ]
 };
 
+console.log('%c*** settings ***', 'background: #00FFFF; color: #FFFFFF; font-weight: bold;');
+console.log(settings);
+
 const chart = picasso.chart({
-  element: document.querySelector('#container'),
+  element: document.querySelector("#container"),
   data: data,
   settings
 });
